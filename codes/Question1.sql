e1c093e1ec33d28e5b7fc315fcc5762232e8be66
lyk6666 <liyi0084@e.ntu.edu.sg>
2025-03-28 12:43:49 +0800

    Previous solutions are wrong. Now fixed.

--- codes/Question1.sql
+++ codes/Question1.sql
@@ -1,13 +1,38 @@
 SELECT * FROM INVESTOR
 SELECT * FROM PORTFOLIO
 
--- Average Loss means annualized return is negative
+DROP VIEW IF EXISTS Latest_Invested_Value
+GO
 
-SELECT Name AS average_loss_across_all_portfolios
-FROM INVESTOR AS I JOIN PORTFOLIO AS P
-ON I.Phone = P.Phone
-WHERE P.Phone NOT IN (
-	SELECT Phone
-	FROM PORTFOLIO
-	WHERE AnnualizedReturn >= 0
-)
+CREATE VIEW Latest_Invested_Value AS
+SELECT 
+    I.Phone, 
+    I.PID, 
+	I.Date,
+    I.Amount
+FROM INVESTED_VALUE AS I
+JOIN (
+    -- Subquery to get the latest date for each (Phone, PID)
+    SELECT Phone, PID, MAX(Date) AS LatestDate
+    FROM INVESTED_VALUE
+    GROUP BY Phone, PID
+) AS Latest ON I.Phone = Latest.Phone 
+            AND I.PID = Latest.PID 
+            AND I.Date = Latest.LatestDate;
+GO
+
+SELECT * FROM Latest_Invested_Value
+ORDER BY Phone, PID
+
+
+WITH T AS(
+	SELECT P.Phone, P.PID, (P.MarketValue - LIV.Amount) AS gain_loss
+	FROM INVESTOR AS I 
+	JOIN PORTFOLIO AS P
+	ON I.Phone = P.Phone
+	JOIN Latest_Invested_Value AS LIV
+	ON LIV.Phone = P.Phone AND LIV.PID = P.PID)
+SELECT I.Name, SUM(T.gain_loss) AS total_gain_loss
+FROM INVESTOR AS I JOIN T ON I.Phone = T.Phone
+GROUP BY I.Name
+HAVING SUM(T.gain_loss) < 0
