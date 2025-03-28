499bbf0ec62f745e4321b8d77beb543d8ba627df
lyk6666 <liyi0084@e.ntu.edu.sg>
2025-03-28 13:36:07 +0800

    add CAST function to ensure formatted numeric value

--- codes/Question3.sql
+++ codes/Question3.sql
@@ -1,6 +1,8 @@
 SELECT * FROM UNREALIZED_GAIN_LOSS
 
-SELECT SUM(Amount)/COUNT(*) AS average_unrealized_gain_loss, SUM(Amount) AS each_month_sum,COUNT(*) AS portfolio_number
+SELECT CAST(SUM(Amount)/COUNT(*) AS DECIMAL(10,2)) AS average_unrealized_gain_loss, 
+SUM(Amount) AS each_month_sum,
+COUNT(*) AS portfolio_number
 FROM UNREALIZED_GAIN_LOSS
 WHERE YEAR(Date) = 2024
 GROUP BY MONTH(Date)
